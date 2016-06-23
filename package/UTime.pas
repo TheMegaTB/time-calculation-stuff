c8728775890b7fcd6d79474e020a3acef75e02cf
Til Blechschmidt <til@blechschmidt.de>
2016-06-23 08:47:58 +0200

    Added str_to_time and its sibbling

--- package/UTime.pas
+++ package/UTime.pas
@@ -1,7 +1,19 @@
 unit UTime;
 
 interface
+  function str_to_time(str: String; delta: Boolean);
+  function time_to_str(time: TTime; delta: Boolean);
 
 implementation
+
+function str_to_time(str: String; delta: Boolean);
+begin
+  //DO NOT DELETE THIS STUPID DELPHI
+end;
+
+function time_to_str(time: TTime; delta: Boolean);
+begin
+  //DO NOT DELETE THIS STUPID DELPHI
+end;
 
 end.
