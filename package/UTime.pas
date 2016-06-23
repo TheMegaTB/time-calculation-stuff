82dba92ddd32d133bdfd93b3d02838227ed2daf6
Til Blechschmidt <til@blechschmidt.de>
2016-06-23 09:19:04 +0200

    Added string parsing

--- package/UTime.pas
+++ package/UTime.pas
@@ -2,6 +2,8 @@ unit UTime;
 
 interface
 
+uses SysUtils;
+
 type
   TTime = LongInt;
 
@@ -12,7 +14,15 @@ const
   DAY = HOUR * 24;
   YEAR = DAY * 365;
   //TTime=0 => 0.0.0000 00:00:00:0000
-  DEFAULT_FORMAT = 'D.M.YYYY HH.MM.SS:mmmm';
+  FMT_DEFAULT = 'DD.MM.YYYY HH.MM.SS:mmmm';
+  FMT_DAY = 'DD';
+  FMT_MONTH = 'MM';
+  FMT_YEAR = 'YYYY';
+  FMT_SHORT_YEAR = 'YY';
+  FMT_HOUR = 'HH';
+  FMT_MINUTE = 'MM';
+  FMT_SECOND = 'SS';
+  FMT_MILLI = 'mmmm';
 
 function str_to_time(str: String; delta: Boolean);
 function time_to_str(time: TTime; delta: Boolean; format: String): String; overload;
@@ -33,12 +43,21 @@ end;
 
 function time_to_str(time: TTime; delta: Boolean; format: String): String; overload;
 begin
-  //DO NOT DELETE THIS STUPID DELPHI
+  StringReplace(format, FMT_DAY, get_day(time));
+  StringReplace(format, FMT_MONTH, get_month(time));
+
+  StringReplace(format, FMT_YEAR, get_year(time));
+  StringReplace(format, FMT_SHORT_YEAR, get_short_year(time));
+
+  StringReplace(format, FMT_HOUR, get_hour(time));
+  StringReplace(format, FMT_MINUTE, get_minute(time));
+  StringReplace(format, FMT_SECOND, get_second(time));
+  StringReplace(format, FMT_MILLI, get_milli(time));
 end;
 
 function time_to_str(time: TTime; delta: Boolean): String; overload;
 begin
-  result:=time_to_str(time, delta, DEFAULT_FORMAT);
+  result:=time_to_str(time, delta, FMT_DEFAULT);
 end;
 
 end.
